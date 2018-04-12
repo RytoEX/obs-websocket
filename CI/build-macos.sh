4882129930f019712bc32cea3ce5fa65f4d4f000
Ryan Foster <RytoEX@gmail.com>
2018-04-12 11:41:01 -0400

    another try

--- CI/build-macos.sh
+++ CI/build-macos.sh
@@ -5,7 +5,7 @@ set -ex
 
 echo obsPath: $obsPath
 if [ -d "$PWD/../obs-studio/libobs" ]; then echo LIBOBS_INCLUDE_DIR exists; fi
-if [ -f "$PWD/../obs-studio/libobs/build/libobs/libobs.dylib" ]; then echo LIBOBS_LIB exists; fi
+if [ -f "$PWD/../obs-studio/build/libobs/libobs.dylib" ]; then echo LIBOBS_LIB exists; fi
 if [ -f "$PWD/../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" ]; then echo OBS_FRONTEND_LIB exists; fi
 
 find "$HOME" -name obs.dylib
@@ -14,12 +14,12 @@ find "$HOME" -name libobs.dylib
 
 mkdir build && cd build
 if [ -d "$PWD/../../obs-studio/libobs" ]; then echo LIBOBS_INCLUDE_DIR exists; fi
-if [ -f "$PWD/../../obs-studio/libobs/build/libobs/libobs.dylib" ]; then echo LIBOBS_LIB exists; fi
+if [ -f "$PWD/../../obs-studio/build/libobs/libobs.dylib" ]; then echo LIBOBS_LIB exists; fi
 if [ -f "$PWD/../../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" ]; then echo OBS_FRONTEND_LIB exists; fi
 
 cmake -DQTDIR=/usr/local/opt/qt \
   -DLIBOBS_INCLUDE_DIR="$(pwd)/../../obs-studio/libobs" \
-  -DLIBOBS_LIB="$(pwd)/../../obs-studio/libobs/build/libobs/libobs.dylib" \
+  -DLIBOBS_LIB="$(pwd)/../../obs-studio/build/libobs/libobs.dylib" \
   -DOBS_FRONTEND_LIB="$(pwd)/../../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DCMAKE_INSTALL_PREFIX=/usr \
