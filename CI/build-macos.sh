0c8de33cd424052874c49c7ada96c02ede61b1ad
Ryan Foster <RytoEX@gmail.com>
2018-04-12 12:47:02 -0400

    plz work

--- CI/build-macos.sh
+++ CI/build-macos.sh
@@ -5,13 +5,15 @@ set -ex
 
 echo obsPath: $obsPath
 if [ -d "$PWD/../obs-studio/libobs" ]; then echo LIBOBS_INCLUDE_DIR exists; fi
+if [ -d "$PWD/../obs-studio/build/libobs" ]; then echo LibObs_DIR exists; fi
+if [ -f "$PWD/../obs-studio/build/libobs/LibObsConfig.cmake" ]; then echo LibObsConfig.cmake exists; fi
 if [ -f "$PWD/../obs-studio/build/libobs/libobs.dylib" ]; then echo LIBOBS_LIB exists; fi
 if [ -f "$PWD/../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" ]; then echo OBS_FRONTEND_LIB exists; fi
 
 mkdir build && cd build
 cmake -DQTDIR=/usr/local/opt/qt \
   -DLIBOBS_INCLUDE_DIR=../../obs-studio/libobs \
-  -DLIBOBS_LIB=../../obs-studio/libobs \
+  -DLibObs_DIR=../../obs-studio/build/libobs \
   -DOBS_FRONTEND_LIB="$(pwd)/../../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DCMAKE_INSTALL_PREFIX=/usr \
