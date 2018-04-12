072bae7779a31e1eef94657415fdf7654ba4f138
Ryan Foster <RytoEX@gmail.com>
2018-04-12 11:30:12 -0400

    test find libobs

--- CI/build-macos.sh
+++ CI/build-macos.sh
@@ -8,6 +8,10 @@ if [ -d "$PWD/../obs-studio/libobs" ]; then echo LIBOBS_INCLUDE_DIR exists; fi
 if [ -f "$PWD/../obs-studio/libobs/build/libobs/libobs.dylib" ]; then echo LIBOBS_LIB exists; fi
 if [ -f "$PWD/../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" ]; then echo OBS_FRONTEND_LIB exists; fi
 
+find "$HOME" -name obs.dylib
+find "$HOME" -name libobs.dylib
+
+
 mkdir build && cd build
 if [ -d "$PWD/../../obs-studio/libobs" ]; then echo LIBOBS_INCLUDE_DIR exists; fi
 if [ -f "$PWD/../../obs-studio/libobs/build/libobs/libobs.dylib" ]; then echo LIBOBS_LIB exists; fi
