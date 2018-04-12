249ff83ddc6ad95008f0851ce6051d954eb03caa
Ryan Foster <RytoEX@gmail.com>
2018-04-12 10:07:35 -0400

    CI(macOS): Fix LIBOBS_INCLUDE_DIR path

--- CI/build-macos.sh
+++ CI/build-macos.sh
@@ -5,7 +5,7 @@ set -ex
 
 mkdir build && cd build
 cmake -DQTDIR=/usr/local/opt/qt \
-  -DLIBOBS_INCLUDE_DIR=../../obs-studio/libobs \
+  -DLIBOBS_INCLUDE_DIR="$(pwd)/../../obs-studio/libobs" \
   -DLIBOBS_LIB=../../obs-studio/libobs \
   -DOBS_FRONTEND_LIB="$(pwd)/../../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
