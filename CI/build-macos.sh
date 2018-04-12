e90bf0ecbcd52cd4623e2c16b6739f839784c9d8
Ryan Foster <RytoEX@gmail.com>
2018-04-12 12:21:58 -0400

    fixup more

--- CI/build-macos.sh
+++ CI/build-macos.sh
@@ -8,18 +8,11 @@ if [ -d "$PWD/../obs-studio/libobs" ]; then echo LIBOBS_INCLUDE_DIR exists; fi
 if [ -f "$PWD/../obs-studio/build/libobs/libobs.dylib" ]; then echo LIBOBS_LIB exists; fi
 if [ -f "$PWD/../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" ]; then echo OBS_FRONTEND_LIB exists; fi
 
-find "$HOME" -name obs.dylib
-find "$HOME" -name libobs.dylib
-
 
 mkdir build && cd build
-if [ -d "$PWD/../../obs-studio/libobs" ]; then echo LIBOBS_INCLUDE_DIR exists; fi
-if [ -f "$PWD/../../obs-studio/build/libobs/libobs.dylib" ]; then echo LIBOBS_LIB exists; fi
-if [ -f "$PWD/../../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" ]; then echo OBS_FRONTEND_LIB exists; fi
-
 cmake -DQTDIR=/usr/local/opt/qt \
-  -DLIBOBS_INCLUDE_DIR="$(pwd)/../../obs-studio/libobs" \
-  -DLIBOBS_LIB="$(pwd)/../../obs-studio/build/libobs/libobs.dylib" \
+  -DLIBOBS_INCLUDE_DIR=../../obs-studio/libobs \
+  -DLIBOBS_LIB=../../obs-studio/libobs \
   -DOBS_FRONTEND_LIB="$(pwd)/../../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DCMAKE_INSTALL_PREFIX=/usr \
