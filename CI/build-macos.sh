ea3822f6ce34202496fa59dff7d46f467e425038
Ryan Foster <RytoEX@gmail.com>
2018-04-14 06:02:40 -0400

    so confused

--- CI/build-macos.sh
+++ CI/build-macos.sh
@@ -45,7 +45,7 @@ mkdir build && cd build
 cmake .. \
   -DQTDIR=/usr/local/opt/qt \
   -DLIBOBS_INCLUDE_DIR=../../obs-studio/libobs \
-  -DLIBOBS_LIB=../../obs-studio/libobs \
+  -DLIBOBS_LIB="$(pwd)/../../obs-studio/build/libobs/libobs.dylib" \
   -DOBS_FRONTEND_LIB="$(pwd)/../../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DCMAKE_INSTALL_PREFIX=/usr \
